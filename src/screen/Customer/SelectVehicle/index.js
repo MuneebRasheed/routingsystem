a0bd9c0df1309147b8e2851bbce83511c0bcebeb
MuneebRasheed <rasheedmuneeb754@gmail.com>
2023-05-02 15:33:29 +0500

    jsonstructure made

--- src/screen/Customer/SelectVehicle/index.js
+++ src/screen/Customer/SelectVehicle/index.js
@@ -10,7 +10,7 @@ import Header from "@component/Header";
 import Accordion from "./Accordion";
 import Modal from "react-native-modalbox";
 import DropDownPicker from "react-native-dropdown-picker";
-import DocumentPicker from 'react-native-document-picker';
+import DocumentPicker from "react-native-document-picker";
 import CalendarStrip from "react-native-calendar-strip";
 
 import { navigate } from "@navigation";
@@ -32,7 +32,7 @@ function SelectVehicle(params) {
   const [height, setHeight] = useState();
   const [length, setLength] = useState();
   const [weight, setWeight] = useState();
-  const [mainModel,setMainModel]=useState(false);
+  const [mainModel, setMainModel] = useState(false);
   const [openModel, setOpenModel] = useState(false);
   const [items, setItems] = useState([
     { label: "Solid", value: "solid" },
@@ -41,7 +41,67 @@ function SelectVehicle(params) {
     { label: "Fragile", value: "fragile" },
     { label: "Other Items", value: "otherItems" },
   ]);
-  const[itemsType,setItemsType]=useState("solid")
+  const [itemsType, setItemsType] = useState("solid");
+
+  const [Structure, setStructure] = useState([
+    {
+      id: 1,
+      image: require("@asset/images/avatar.png"),
+      vehicalName: "Suzuki Wagon R",
+      cityName: "Lahore",
+      rating: "4.8",
+      totalRides: "2454",
+      price: "650",
+      time: "2",
+      distance: "406",
+      userId: "123",
+      tripId: "1",
+      IsActive: true,
+      IsBidding: true,
+    },
+    {
+      id: 2,
+      image: require("@asset/images/avatar.png"),
+      vehicalName: "Cultus",
+      cityName: "Lahore",
+      rating: "4.8",
+      totalRides: "2454",
+      price: "650",
+      time: "2",
+      distance: "406",
+      userId: "123",
+      tripId: "1",
+      IsActive: true,
+      IsBidding: false,
+    },
+    {
+      id: 3,
+      image: require("@asset/images/avatar.png"),
+      vehicalName: "Civic",
+      cityName: "Lahore",
+      rating: "4.8",
+      totalRides: "2454",
+      price: "650",
+      time: "23",
+      distance: "4000",
+      userId: "123",
+      tripId: "1",
+      IsActive: true,
+      IsBidding: false,
+    },
+  ]);
+
+  function CloseModelBaseOnId(id) {
+    console.log("Here Is Id ", Structure.length);
+    if (Structure.length == 1) {
+      setMainModel(false);
+    }
+    setStructure((previous) => {
+      return previous.filter((value) => {
+        return value.id != id;
+      });
+    });
+  }
 
   const ModalNotification = useRef();
   const getPhotoFromCamera = () => {
@@ -53,15 +113,13 @@ function SelectVehicle(params) {
       console.log(image);
     });
   };
-  const UploadData=async()=>{
+  const UploadData = async () => {
     try {
       const res = await DocumentPicker.pick({
         type: [DocumentPicker.types.allFiles],
       });
-      setImages(res[0])
-      console.log(
-        res[0]
-      );
+      setImages(res[0]);
+      console.log(res[0]);
     } catch (err) {
       if (DocumentPicker.isCancel(err)) {
         // User cancelled the picker, exit any dialogs or menus and move on
@@ -69,11 +127,9 @@ function SelectVehicle(params) {
         throw err;
       }
     }
+  };
 
-    
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     // getFCMToken();
     messaging().onNotificationOpenedApp((remoteMessage) => {
       console.log(
@@ -93,141 +149,146 @@ function SelectVehicle(params) {
         }
       });
     messaging().onMessage(async (remoteMessage) => {
-      setMainModel(pre=>!pre)
-      console.log(" on delected vehical notification on foreground state....", remoteMessage);
+      setMainModel((pre) => !pre);
+      console.log(
+        " on delected vehical notification on foreground state....",
+        remoteMessage
+      );
     });
+  }, []);
 
-  },[])
-
-  const MainModel =()=>{
-
-    return(<Modal
-      ref={ModalNotification}
-      isOpen={true}
-      entry={"top"}
-      swipeToClose={false}
-      style={{ height:180,width:400,borderRadius:10,alignItems:'center'}}
-      backdropPressToClose={false}
-    >
-      <View style={{borderRadius:50}}>
-        <View  style={{flexDirection:'row'}}>
-          <View style={{width:'20%'}}>
-          <Image source={require('@asset/images/avatar.png')} resizeMode='cover' style={{width:50,height:50,borderRadius:25,margin:10}} />
-           
+  const MainModel = ({value}) => {
+    return (
+      <Modal
+        ref={ModalNotification}
+        isOpen={true}
+        entry={"top"}
+        swipeToClose={false}
+        style={{
+          height: 180,
+          width: 400,
+          borderRadius: 10,
+          alignItems: "center",
+        }}
+        swipeArea={300}
+        backdropPressToClose={false}
+      >
+        <View style={{ borderRadius: 50 }}>
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ width: "20%" }}>
+              <Image
+                source={value.image}
+                resizeMode="cover"
+                style={{ width: 50, height: 50, borderRadius: 25, margin: 10 }}
+              />
+            </View>
+            <View style={{ width: "80%" }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  margin: 5,
+                  fontSize: 20,
+                }}
+              >
+                <Text>{__(value.vehicalName)}</Text>
+                <Text>{__(value.price)}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  margin: 5,
+                  fontSize: 20,
+                }}
+              >
+                <Text>{__("Muzafar")}</Text>
+                <Text>{__(value.time)}</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  margin: 5,
+                  fontSize: 20,
+                }}
+              >
+                <Text>{__(value.rating+`(${value.totalRides})`)}</Text>
+                <Text>{__(value.distance)}</Text>
+              </View>
+            </View>
           </View>
-          <View style={{width:'80%'}}>
-            <View style={{flexDirection:'row',justifyContent:'space-between',margin:5,fontSize:20}}>
-            <Text >
-              {__("Suzuki Wagon R")}
-            </Text>
-            <Text >
-              {__("PKR 650)")}
-            </Text>
-
-            </View>
-            <View style={{flexDirection:'row',justifyContent:'space-between',margin:5,fontSize:20}}>
-            <Text >
-              {__("Muzafar")}
-            </Text>
-            <Text >
-              {__("2 min)")}
-            </Text>
-              
-            </View>
-            <View style={{flexDirection:'row',justifyContent:'space-between',margin:5,fontSize:20}}>
-            <Text >
-              {__("4.8(2454)")}
-            </Text>
-            <Text >
-              {__("406 m)")}
-            </Text>
-              
-            </View>
-          </View>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
-          <Button
-            style={[styles.bookingBtn, { width: "40%" }]}
-            onPress={() => {
-              // navigate("CustomerPayment");
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
             }}
           >
-            <Text style={styles.bookingBtnText}>{__("Decline")}</Text>
-          </Button>
-          <Button
-            style={[styles.bookingBtn, { width: "40%" }]}
-            onPress={() => {
-              navigate("CustomerPayment");
-            }}
-          >
-            <Text style={styles.bookingBtnText}>{__("Accept")}</Text>
-          </Button>
+            <Button
+              style={[styles.bookingBtn, { width: "40%" }]}
+              onPress={() => {
+                // navigate("CustomerPayment");
+                CloseModelBaseOnId(value.id);
+              }}
+            >
+              <Text style={styles.bookingBtnText}>{__("Decline")}</Text>
+            </Button>
+            <Button
+              style={[styles.bookingBtn, { width: "40%" }]}
+              onPress={() => {
+                navigate("CustomerPayment");
+              }}
+            >
+              <Text style={styles.bookingBtnText}>{__("Accept")}</Text>
+            </Button>
+          </View>
         </View>
-      </View>
-    </Modal>)
-  }
+      </Modal>
+    );
+  };
   const getPhotoFromGallery = () => {
-    // ImagePicker.openPicker({
-    //   width: 300,
-    //   height: 400,
-    //   cropping: true
-    // }).then(image => {
-    //   console.log(image);
-    // });
-
-    // ImagePicker.openPicker({
-    //   multiple: true,
-    // }).then((image) => {
-    //   console.log(image);
-    //   setImages(image);
-    // });
     UploadData();
   };
 
-const fetchData = async () => {
-    
+  const fetchData = async () => {
     var data = await AsyncStorage.getItem("response");
     var datas = JSON.parse(data);
 
-
     var fcmToken = await AsyncStorage.getItem("fcmtoken");
     // var fcmToken1 = JSON.parse(fcmToken);
-    console.log(fcmToken)
-const formData= new FormData()
-formData.append("files",images)
-formData.append("from_location",JSON.stringify(params.route.params.form))
-formData.append("to_location",JSON.stringify(params.route.params.to))
-formData.append("height",height)
-formData.append("fare",fare)
-formData.append("width",width)
-formData.append("time","2023-12-06")
-formData.append("length",length)
-formData.append("weight",weight)
-formData.append("parcel_type","wood")
+    console.log(fcmToken);
+    const formData = new FormData();
+    formData.append("files", images);
+    formData.append("from_location", JSON.stringify(params.route.params.form));
+    formData.append("to_location", JSON.stringify(params.route.params.to));
+    formData.append("height", height);
+    formData.append("fare", fare);
+    formData.append("width", width);
+    formData.append("time", "2023-12-06");
+    formData.append("length", length);
+    formData.append("weight", weight);
+    formData.append("parcel_type", "wood");
 
-console.log("FormData",formData);
+    console.log("FormData", formData);
 
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    Authorization: `Bearer ${datas.access_token}`,
-    'Content-Type':'multipart/form-data'
-  },
-  body: formData
-};
-try {
-  const res = await fetch('https://testing.explorelogix.com/v1/parcel', requestOptions);
-  const result = await res.json();
-  console.log('RESULT', result);
-} catch (err) {
-  console.log('ERROR');
-}
-
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${datas.access_token}`,
+        "Content-Type": "multipart/form-data",
+      },
+      body: formData,
+    };
+    try {
+      const res = await fetch(
+        "https://testing.explorelogix.com/v1/parcel",
+        requestOptions
+      );
+      const result = await res.json();
+      console.log("RESULT", result);
+    } catch (err) {
+      console.log("ERROR");
+    }
   };
 
   async function onDisplayNotification() {
@@ -306,45 +367,40 @@ try {
   }
   return (
     <Container style={theme.layoutFx}>
-        <Modal
-         isOpen={mainModel}
-         entry={"top"}
-         backdropOpacity={0.3}
-         >
-         <View style={{height:'30%'}}>
-         <MainModel/>
+      <Modal isOpen={mainModel} entry={"top"}  backdropOpacity={0.3} swipeToClose={false} >
+
+        {Structure.map(val=>{
+          return(
+            val.IsActive &&<View style={{ height: "30%" }}>
+            <MainModel value={val} />
           </View>
-          <View style={{height:'30%'}}>
-          <MainModel/>
-          </View>
-          <View style={{height:'30%'}}>
-          <MainModel/>
-          </View>
-      
-          <Button
-                style={[styles.bookingBtn,{backgroundColor:'grey'}]}
-                onPress={() => {
-                  // getPhotoFromGallery();
-                  // navigate("CustomerPayment");
-                  setMainModel(false);
-                }}
-              >
-                <Text style={styles.bookingBtnText}>
-                  {__("Cancel")}
-                </Text>
-              </Button>
-        </Modal>
+          )
+        }
+          
+        )}
+       
+       
+        
+
+
+        <Button
+          style={[styles.bookingBtn, { backgroundColor: "grey" }]}
+          onPress={() => {
+            // getPhotoFromGallery();
+            // navigate("CustomerPayment");
+            setMainModel(false);
+          }}
+        >
+          <Text style={styles.bookingBtnText}>{__("Cancel")}</Text>
+        </Button>
+      </Modal>
       <DarkStatusBar />
       <Header leftType="back" title={"Booking"} />
 
       <Content contentContainerStyle={theme.layoutDf}>
-     
-     
-            
         <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={[styles.selectVehicleContainer,{height:600}]}>
+          <View style={[styles.selectVehicleContainer, { height: 600 }]}>
             <View style={styles.selectVehicleContent}>
-              
               <Accordion
                 title="Select Time"
                 renderContent={() => (
@@ -438,21 +494,18 @@ try {
                   style={[styles.formInput, styles.formInput2]}
                 />
               </View>
-              
-                    
-                      <DropDownPicker
-                        open={openModel}
-                        items={items}
-                        setOpen={setOpenModel}
-                        value={itemsType}
-                        onSelectItem={(e) => setItemsType(e.value)}
-                        // setValue={handleChange("gender")}
-                        setItems={setItems}
-                        style={{marginBottom:5,borderWidth:0}}
-                      />
-                     
-                  
-                  
+
+              <DropDownPicker
+                open={openModel}
+                items={items}
+                setOpen={setOpenModel}
+                value={itemsType}
+                onSelectItem={(e) => setItemsType(e.value)}
+                // setValue={handleChange("gender")}
+                setItems={setItems}
+                style={{ marginBottom: 5, borderWidth: 0 }}
+              />
+
               <Button
                 style={styles.bookingBtn}
                 onPress={() => {
@@ -465,16 +518,14 @@ try {
                 </Text>
               </Button>
             </View>
-            
-            
           </View>
         </ScrollView>
       </Content>
       <Button
         style={styles.bookingBtn}
         onPress={() => {
-          fetchData()
-          // setMainModel(true)
+          // fetchData()
+          setMainModel(true);
         }}
       >
         <Text style={styles.bookingBtnText}>{__("BOOK NOW")}</Text>
